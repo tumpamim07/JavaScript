60debb30753ed9216b2cee0add8e07e53e4ba3cf
Tumpa Moni Mim <tumpamoni455@gmail.com>
2024-11-11 22:44:44 +0600

    Keyword, Data Type, Variable Declaration,Number Method
    -Keyword
    -Data Type (String, Number, Boolean)
    -typeof() function
    
    -Variable Declaration and Rules
    
    -Number Method (Conversion)
    -tostring
    -parseInt
    -parseFloat
    -toFixed
    -toPrecision

---
+++ Day-02 (Keyword, Data Type, Variable Declaration,Number Method)/index.js
@@ -0,0 +1,39 @@
+//Convert any data type to string
+var num = 20;
+num=toString(num);
+console.log(typeof(num))
+
+
+//Convert any data type to integer
+
+var name = "20";
+variable = parseInt(name);
+console.log(typeof(variable))
+
+
+//Convert any data type to float
+
+var name = 20.464;
+variable = parseFloat(name);
+console.log(typeof(variable))
+
+
+var num = 20.464;
+console.log(num.toFixed(3)) //toFixed -- show number after point (.)
+
+
+var num = 20.574689
+console.log(num.toPrecision(5)) //toPrecision -- show total number of giver number
+
+//Convert any data type to number
+
+console.log(typeof (Number("123")))
+console.log(Number("123"))
+
+
+//Convert any data type to boolean
+
+console.log(typeof(false))
+
+console.log(Number(false))
+console.log(Number(true))
